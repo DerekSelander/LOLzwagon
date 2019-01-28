a3b5178b62b7ff6bd4aa729af834ed7d96d0fb1f
Derek Selander <derekselander@yahoo.com>
2019-01-27 20:33:04 -0700

    https://giphy.com/gifs/9K2nFglCAQClO

---
+++ CodeCoverageTests/ObjcCoverageTests.m
@@ -0,0 +1,66 @@
+//
+//  ObjcCoverageTests.m
+//  CodeCoverageTests
+//
+//  Created by Derek Selander on 1/27/19.
+//  Copyright © 2019 Derek Selander. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ObjcCoverageTests : XCTestCase
+
+@end
+
+@implementation ObjcCoverageTests
+
+- (void)testAssertNil {
+    XCTAssertNil([UIViewController new], @"UIViewController should be nil");
+}
+
+- (void)testFail {
+    XCTFail(@"womp womp. I spilled my ice cream");
+}
+
+- (void)testAssertNotNil {
+    XCTAssertNotNil(nil, "This clearly should be nil");
+}
+
+- (void)testAssert {
+    XCTAssert(3==6, @"3 should equal 6");
+}
+
+- (void)testAssertTrue {
+    XCTAssertTrue(NO, "No should equal YES");
+}
+
+- (void)testAssertFalse {
+    XCTAssertFalse(YES, "YES should equal NO");
+}
+
+- (void)testAssertEqualObjects {
+    XCTAssertEqualObjects(@"Yep", @"Nope", @"\"Yep\" and \"Nope\" should be equal");
+}
+
+- (void)testAssertNotEqualObjects {
+    XCTAssertNotEqualObjects(@55, @55, @"55 should not equal 55");
+}
+
+- (void)testAssertEqual {
+    XCTAssertEqual(23, 45, @"23 == 45");
+}
+
+- (void)testAssertNotEqual {
+    XCTAssertNotEqual(420, 420, @"whaaaaaaaaaa.... where am I?");
+}
+
+- (void)testAssertEqualWithAccuracy {
+    XCTAssertEqualWithAccuracy(23, 54, 0);
+}
+
+- (void)testAssertNotEqualWithAccuracy {
+    XCTAssertEqualWithAccuracy(54, 54, 100);
+}
+
+
+@end
--- LOLzwagon/doIt.c
+++ LOLzwagon/doIt.c
@@ -20,6 +20,14 @@
 // CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 // OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//  doIt.c
+//  LOLzwagon
+//
+//  Created by Derek Selander on 1/27/19.
+//  Copyright © 2019 Derek Selander. All rights reserved.
+//
+
 
 #ifndef fishhook_h
 #define fishhook_h
@@ -97,15 +105,16 @@ extern "C" {
 // OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-
-
 #include <dlfcn.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #include <sys/types.h>
 #include <mach-o/dyld.h>
 #include <mach-o/loader.h>
+#include <mach-o/getsect.h>
 #include <mach-o/nlist.h>
+#include <libgen.h>
 
 #ifdef __LP64__
 typedef struct mach_header_64 mach_header_t;
@@ -131,29 +140,9 @@ struct rebindings_entry {
     struct rebindings_entry *next;
 };
 
-static struct rebindings_entry *_rebindings_head;
+__attribute((used)) static void noOpFunction() { }
 
-static int prepend_rebindings(struct rebindings_entry **rebindings_head,
-                              struct rebinding rebindings[],
-                              size_t nel) {
-    struct rebindings_entry *new_entry = (struct rebindings_entry *) malloc(sizeof(struct rebindings_entry));
-    if (!new_entry) {
-        return -1;
-    }
-    new_entry->rebindings = (struct rebinding *) malloc(sizeof(struct rebinding) * nel);
-    if (!new_entry->rebindings) {
-        free(new_entry);
-        return -1;
-    }
-    memcpy(new_entry->rebindings, rebindings, sizeof(struct rebinding) * nel);
-    new_entry->rebindings_nel = nel;
-    new_entry->next = *rebindings_head;
-    *rebindings_head = new_entry;
-    return 0;
-}
-
-static void perform_rebinding_with_section(struct rebindings_entry *rebindings,
-                                           section_t *section,
+static void rebind_xctest_functions(section_t *section,
                                            intptr_t slide,
                                            nlist_t *symtab,
                                            char *strtab,
@@ -168,29 +157,32 @@ static void perform_rebinding_with_section(struct rebindings_entry *rebindings,
         }
         uint32_t strtab_offset = symtab[symtab_index].n_un.n_strx;
         char *symbol_name = strtab + strtab_offset;
+        
         bool symbol_name_longer_than_1 = symbol_name[0] && symbol_name[1];
-        struct rebindings_entry *cur = rebindings;
-        while (cur) {
-            for (uint j = 0; j < cur->rebindings_nel; j++) {
-                if (symbol_name_longer_than_1 &&
-                    strcmp(&symbol_name[1], cur->rebindings[j].name) == 0) {
-                    if (cur->rebindings[j].replaced != NULL &&
-                        indirect_symbol_bindings[i] != cur->rebindings[j].replacement) {
-                        *(cur->rebindings[j].replaced) = indirect_symbol_bindings[i];
-                    }
-                    indirect_symbol_bindings[i] = cur->rebindings[j].replacement;
-                    goto symbol_loop;
-                }
-            }
-            cur = cur->next;
+        char *needle = strnstr(symbol_name, "_$S6XCTest", 10);
+        
+        
+        if (!symbol_name_longer_than_1) {
+            continue;
         }
-    symbol_loop:;
+        if (strcmp(symbol_name, "__XCTFailureHandler") == 0) {
+             indirect_symbol_bindings[i] = &noOpFunction;
+            printf("yay\n");
+        }
+        else if (!needle || !strstr(&symbol_name[6], "XCT")) {
+            continue;
+        }
+   
+        indirect_symbol_bindings[i] = &noOpFunction;
+//        printf("%s\n", symbol_name);
+        
+       
     }
 }
 
-static void rebind_symbols_for_image(struct rebindings_entry *rebindings,
-                                     const struct mach_header *header,
-                                     intptr_t slide) {
+static void rebind_xctest_symbols_for_image(const struct mach_header *header,
+                                     intptr_t slide ) {
+    
     Dl_info info;
     if (dladdr(header, &info) == 0) {
         return;
@@ -202,6 +194,7 @@ static void rebind_symbols_for_image(struct rebindings_entry *rebindings,
     struct dysymtab_command* dysymtab_cmd = NULL;
     
     uintptr_t cur = (uintptr_t)header + sizeof(mach_header_t);
+    bool linkdsToXCTestOrlibSwiftXCTest = false;
     for (uint i = 0; i < header->ncmds; i++, cur += cur_seg_cmd->cmdsize) {
         cur_seg_cmd = (segment_command_t *)cur;
         if (cur_seg_cmd->cmd == LC_SEGMENT_ARCH_DEPENDENT) {
@@ -212,10 +205,24 @@ static void rebind_symbols_for_image(struct rebindings_entry *rebindings,
             symtab_cmd = (struct symtab_command*)cur_seg_cmd;
         } else if (cur_seg_cmd->cmd == LC_DYSYMTAB) {
             dysymtab_cmd = (struct dysymtab_command*)cur_seg_cmd;
+        } else if (cur_seg_cmd->cmd == LC_LOAD_DYLIB) {
+            struct dylib_command *dyld_cmd = (struct dylib_command *)cur_seg_cmd;
+#ifdef __LP64__
+            char *libraryPath = (char *)(cur + dyld_cmd->dylib.name.offset);
+#else
+            char *libraryPath = dyld_cmd->dylib.name.ptr;
+#endif
+            
+            char * libraryName = basename(libraryPath);
+            if (strcmp(libraryName, "libswiftXCTest.dylib") == 0
+                || strcmp(libraryName, "XCTest") == 0) {
+                linkdsToXCTestOrlibSwiftXCTest = true;
+            }
+            
         }
     }
     
-    if (!symtab_cmd || !dysymtab_cmd || !linkedit_segment ||
+    if (!linkdsToXCTestOrlibSwiftXCTest || !symtab_cmd || !dysymtab_cmd || !linkedit_segment ||
         !dysymtab_cmd->nindirectsyms) {
         return;
     }
@@ -240,101 +247,39 @@ static void rebind_symbols_for_image(struct rebindings_entry *rebindings,
                 section_t *sect =
                 (section_t *)(cur + sizeof(segment_command_t)) + j;
                 if ((sect->flags & SECTION_TYPE) == S_LAZY_SYMBOL_POINTERS) {
-                    perform_rebinding_with_section(rebindings, sect, slide, symtab, strtab, indirect_symtab);
+                    rebind_xctest_functions(sect, slide, symtab, strtab, indirect_symtab);
                 }
                 if ((sect->flags & SECTION_TYPE) == S_NON_LAZY_SYMBOL_POINTERS) {
-                    perform_rebinding_with_section(rebindings, sect, slide, symtab, strtab, indirect_symtab);
+                    rebind_xctest_functions(sect, slide, symtab, strtab, indirect_symtab);
                 }
             }
         }
     }
 }
 
-static void _rebind_symbols_for_image(const struct mach_header *header,
-                                      intptr_t slide) {
-    rebind_symbols_for_image(_rebindings_head, header, slide);
-}
-
-int rebind_symbols_image(void *header,
-                         intptr_t slide,
-                         struct rebinding rebindings[],
-                         size_t rebindings_nel) {
-    struct rebindings_entry *rebindings_head = NULL;
-    int retval = prepend_rebindings(&rebindings_head, rebindings, rebindings_nel);
-    rebind_symbols_for_image(rebindings_head, (const struct mach_header *) header, slide);
-    if (rebindings_head) {
-        free(rebindings_head->rebindings);
-    }
-    free(rebindings_head);
-    return retval;
-}
-
-int rebind_symbols(struct rebinding rebindings[], size_t rebindings_nel) {
-    int retval = prepend_rebindings(&_rebindings_head, rebindings, rebindings_nel);
-    if (retval < 0) {
-        return retval;
-    }
-    // If this was the first call, register callback for image additions (which is also invoked for
-    // existing images, otherwise, just run on existing images
-    if (!_rebindings_head->next) {
-        _dyld_register_func_for_add_image(_rebind_symbols_for_image);
-    } else {
-        uint32_t c = _dyld_image_count();
-        for (uint32_t i = 0; i < c; i++) {
-            _rebind_symbols_for_image(_dyld_get_image_header(i), _dyld_get_image_vmaddr_slide(i));
-        }
-    }
-    return retval;
-}
-
-
-
-//
-//  doIt.c
-//  LOLzwagon
-//
-//  Created by Derek Selander on 1/27/19.
-//  Copyright © 2019 Derek Selander. All rights reserved.
-//
-#include <mach-o/getsect.h>
-
-
-
-
 static void lolzwagon(const struct mach_header *header, intptr_t slide) {
+    
 #ifdef __LP64__
     uint64_t size = 0;
     char *data = getsectdatafromheader_64((const struct mach_header_64*)header, "__DATA", "__llvm_prf_cnts", &size) + slide;
 #else
-    
     uint32_t size = 0;
     char *data = getsectdatafromheader(header, "__DATA", "__llvm_prf_cnts", &size) + slide;
 #endif
+    
     if (!size) { return; }
     uintptr_t *ptr = (uintptr_t*)data;
     for (int j = 0; j < size / sizeof(void*); j++) {
+        // array of void* __llvm_prf_cnts, if 1, then that code section has been explored
         ptr[j]++;
     }
-}
-
-
-__unused static void noopFunction() {
     
+    rebind_xctest_symbols_for_image(header, slide);
 }
 
+
+
+/// The fun starts here
 __attribute__((constructor)) static void lets_get_it_started_in_haaaaa(void) {
-
     _dyld_register_func_for_add_image(lolzwagon);
-    for (int i = 0; i < _dyld_image_count(); i++) {
-        const struct mach_header* header = _dyld_get_image_header(i);
-        lolzwagon(header, _dyld_get_image_vmaddr_slide(i));
-    }
-    
-    
-
-//    rebind_symbols((struct rebinding[2]){{"close", noopFunction, (void *)NULL}, {"open", noopFunction, (void *)NULL}}, 2);
-    
 }
-
-
- 
